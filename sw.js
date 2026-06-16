bab80d0e51f59cfeb950ca0348762e63c86d1367
Slet <114405225+PocketBinary@users.noreply.github.com>
2026-06-16 20:50:26 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'pomodoro-v1';
+const ASSETS = [
+  '/pomodoro/',
+  '/pomodoro/index.html',
+  '/pomodoro/script.js',
+  '/pomodoro/pomodoro.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
